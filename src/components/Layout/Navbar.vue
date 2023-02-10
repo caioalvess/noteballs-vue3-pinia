14651e2fa1a352662d998b5eb56823e780a967db
caioalves <caio.oliveira@u4crypto.com>
2023-02-10 20:19:38 -0300

    [REFACTOR] Navbar functionality final design

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -1,3 +1,60 @@
 <template>
-  My Navbar
+  <nav class="navbar is-success" role="navigation" aria-label="main navigation">
+    <div class="container is-max-desktop">
+      <div class="navbar-brand">
+        <div class="navbar-item is-size-4 is-family-monospace">Noteballs</div>
+
+        <a
+          @click.prevent="showMobileNav = !showMobileNav"
+          :class="{ 'is-active': showMobileNav }"
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active': showMobileNav }"
+      >
+        <div class="navbar-end">
+          <div class="navbar-start">
+            <RouterLink to="/" class="navbar-item" active-class="is-active"
+              >Notes</RouterLink
+            >
+            <RouterLink to="/stats" class="navbar-item">Stats</RouterLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
+
+<script setup>
+/*
+IMPORTS
+*/
+import { ref } from "vue";
+
+/*
+  MOBILE NAV
+*/
+const showMobileNav = ref(false);
+</script>
+
+<style>
+  @media(max-width: 1023px) {
+    .navbar-menu {
+      position: absolute;
+      left: 0;
+      width: 100%;
+    }
+  }
+</style>
