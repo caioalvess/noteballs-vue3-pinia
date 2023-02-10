7b4745d9425b5b9ae3b0b2f052d90e32eb3f5402
caioalves <caio.oliveira@u4crypto.com>
2023-02-10 20:28:05 -0300

    [FEAT] Notes design with Bulma cards

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>ViewNotes</div>
+  <div v-for="i in 3" class="card mb-4">
+    <div class="card-content">
+      <div class="content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta porro
+        neque ea distinctio nam veniam laborum aperiam similique magni soluta
+        blanditiis magnam, iusto quam. Ipsa sapiente vero cum similique dolore.
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Save</a>
+      <a href="#" class="card-footer-item">Edit</a>
+    </footer>
+  </div>
 </template>
