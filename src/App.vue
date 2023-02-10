f576dcbb3b408fb3e33de6c51734a9931379c100
caioalves <caio.oliveira@u4crypto.com>
2023-02-10 20:05:40 -0300

    [REFACTOR] Some Bulma navbar styles

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,28 @@
 <template>
-  <!-- <RouterLink to="/" class="button">Notes</RouterLink> |
-  <RouterLink to="/stats" class="button">Stats</RouterLink> -->
-
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-      </a>
-  
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <div class="navbar-item is-size-4 is-family-monospace">
+        Noteballs
+      </div>
+
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
     </div>
-  
+
     <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item">
-          Home
-        </a>
-  
-        <a class="navbar-item">
-          Documentation
-        </a>
-  
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link">
-            More
-          </a>
-  
-          <div class="navbar-dropdown">
-            <a class="navbar-item">
-              About
-            </a>
-            <a class="navbar-item">
-              Jobs
-            </a>
-            <a class="navbar-item">
-              Contact
-            </a>
-            <hr class="navbar-divider">
-            <a class="navbar-item">
-              Report an issue
-            </a>
-          </div>
-        </div>
-      </div>
-  
       <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light">
-              Log in
-            </a>
-          </div>
+        <div class="navbar-start">
+          <RouterLink to="/" class="navbar-item" active-class="is-active">Notes</RouterLink>
+          <RouterLink to="/stats" class="navbar-item">Stats</RouterLink>
         </div>
       </div>
     </div>
@@ -72,7 +37,7 @@
 /*
   IMPORTS
 */
-import Navbar from './components/Layout/Navbar.vue';
+import Navbar from "./components/Layout/Navbar.vue";
 </script>
 
 <style>
