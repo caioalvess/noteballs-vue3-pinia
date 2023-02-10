1ace4fa425eac75a51dff73fc86ff0f8d6209872
caioalves <caio.oliveira@u4crypto.com>
2023-02-10 20:35:57 -0300

    [REFACTOR] Add notes area

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,15 +1,33 @@
 <template>
-  <div v-for="i in 3" class="card mb-4">
-    <div class="card-content">
-      <div class="content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta porro
-        neque ea distinctio nam veniam laborum aperiam similique magni soluta
-        blanditiis magnam, iusto quam. Ipsa sapiente vero cum similique dolore.
+  <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">
+            Add New Note
+          </button>
+        </div>
       </div>
     </div>
-    <footer class="card-footer">
-      <a href="#" class="card-footer-item">Save</a>
-      <a href="#" class="card-footer-item">Edit</a>
-    </footer>
+
+    <div v-for="i in 3" class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa
+          commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim
+          quasi officiis aperiam fugit, corrupti omnis, eaque animi.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
