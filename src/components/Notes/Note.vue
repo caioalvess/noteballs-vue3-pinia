12ff1e51e9efb412907dd4b8c8618e6bc669e922
caioalves <caio.oliveira@u4crypto.com>
2023-02-11 01:59:39 -0300

    [FIX] Wrapping text off card content

---
+++ src/components/Notes/Note.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-content">
+      <div class="content">
+        {{ note.content }}
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+/* 
+PROPS
+*/
+const props = defineProps({
+  note: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style>
+.content {
+    white-space: unset
+}
+</style>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -25,17 +25,8 @@
       </div>
     </div>
 
-    <div v-for="note in notes" :key="note.id" class="card mb-4">
-      <div class="card-content">
-        <div class="content">
-          {{ note.content }}
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    <Note v-for="note in notes" :key="note.id" :note="note" />
+
   </div>
 </template>
 
@@ -44,6 +35,8 @@
 IMPORTS
 */
 import { ref } from "vue";
+
+import Note from "../components/Notes/Note.vue";
 
 /*
 NOTES
@@ -84,3 +77,4 @@ const addNotes = () => {
   newNoteRef.value.focus();
 };
 </script>
+
